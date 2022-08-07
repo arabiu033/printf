df752c519335c2aad95e90c769e74fd29f088351
Aminu Rabiu <arabiu033@gmail.com>
2022-08-07 11:14:42 -0700

    update the binary file

--- printf_bin.c
+++ printf_bin.c
@@ -9,42 +9,23 @@
  */
 int printf_bin(va_list b)
 {
-	unsigned int n;
-	int x, i, temp, count = 0, z, y;
-	int *bin;
+	unsigned int dig, count = 1, len = 0, temp, x;
 
-	n = va_arg(b, int);
-	temp = n;
-	if (n > 1)
+	dig = va_arg(b ,unsigned int);
+	temp = dig;
+
+	while (temp > 1)
 	{
-		while (n > 1)
-		{
-			n = n / 2;
-			count++;
-		}
-		count++;
-		bin = malloc(sizeof(int) * count);
-		if (bin == NULL)
-			free(bin);
-		i = count - 1;
-		y = i;
-		while (temp > 1)
-		{
-			x = temp % 2;
-			temp = temp / 2;
-			bin[i] = x;
-			i--;
-		}
-		bin[i] = temp;
-
-		for (i = 0; i <= y; i++)
-		{
-			z = bin[i];
-			_putchar('0' + z);
-		}
+		temp = temp / 2;
+		count = count * 2;
 	}
-	else
-		_putchar('0' + n);
-
-	return (count);
+	while (count > 0)
+	{
+		x = dig / count;
+		dig = dig % count;
+		count = count / 2;
+		_putchar('0' + x);
+		len++;
+	}
+	return (len);
 }
