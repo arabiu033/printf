b5d79850e2b6092947e986b215ddb4437a2739bc
xz <odufeso1@gmail.com>
2022-08-07 10:49:15 +0100

    Variable update

--- printf_37.c
+++ printf_37.c
@@ -2,11 +2,11 @@
 
 /**
  * printf_37 - prints %
- * @per: ascii 37
+ * @percentage: ascii 37
  *
  * Return: 1
  */
-int printf_37(va_list per __attribute__((unused)))
+int printf_37(va_list percentage __attribute__((unused)))
 {
 	_putchar(37);
 	return (1);
