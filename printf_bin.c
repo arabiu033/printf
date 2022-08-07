a33b1df6674f89f1425e505bbc674ad31d6e6791
xz <odufeso1@gmail.com>
2022-08-07 20:19:51 +0100

    Updated printf_bin.c

--- printf_bin.c
+++ printf_bin.c
@@ -1,50 +1,30 @@
 #include "main.h"
-#include <stdlib.h>
 
 /**
- * printf_bin - does binary conversion
- * @b: digit to convert
+ * printf_bin - prints an int in octal form
+ * @b: int to print
  *
- * Return: No. of digits printed
+ * Return: length of the number printed
  */
 int printf_bin(va_list b)
 {
-	unsigned int n;
-	int x, i, temp, count = 0, z, y;
-	int *bin;
+	unsigned int dig, count = 1, len = 0, temp, x;
 
-	n = va_arg(b, int);
-	temp = n;
-	if (n > 1)
+	dig = va_arg(b, unsigned int);
+	temp = dig;
+
+	while (temp > 1)
 	{
-		while (n > 1)
-		{
-			n = n / 2;
-			count++;
-		}
-		count++;
-		bin = malloc(sizeof(int) * count);
-		if (bin == NULL)
-			free(bin);
-		i = count - 1;
-		y = i;
-		while (temp > 1)
-		{
-			x = temp % 2;
-			temp = temp / 2;
-			bin[i] = x;
-			i--;
-		}
-		bin[i] = temp;
-
-		for (i = 0; i <= y; i++)
-		{
-			z = bin[i];
-			_putchar('0' + z);
-		}
+		temp = temp / 2;
+		count = count * 2;
 	}
-	else
-		_putchar('0' + n);
-
-	return (count);
+	while (count > 0)
+	{
+		x = dig / count;
+		dig = dig % count;
+		count = count / 2;
+		_putchar('0' + x);
+		len++;
+	}
+	return (len);
 }
