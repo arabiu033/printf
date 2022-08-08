86c003d194cce078ee5b52e7a03551045eb3ef5a
xz <odufeso1@gmail.com>
2022-08-08 01:17:24 +0100

    Updated printf_pointer

--- printf_pointer.c
+++ printf_pointer.c
@@ -9,9 +9,18 @@
 int printf_pointer(va_list p)
 {
 	unsigned long int dig;
-	int len = 0;
+	int len = 0, i;
+	char str[6] = "(nil)";
 
 	dig = va_arg(p, unsigned long int);
+
+	if (dig == NULL)
+	{
+		for (i = 0; i < 5; i++)
+			_putchar('0' + str[i]);
+		return (5);
+	}
+	
 	_putchar('0');
 	_putchar('x');
 
