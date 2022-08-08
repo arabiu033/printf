34a8bdc82db0f4356613ba3752ebb6e2c2882f97
xz <odufeso1@gmail.com>
2022-08-09 00:26:08 +0100

    Updates

--- main.c
+++ main.c
@@ -42,7 +42,7 @@ int main(void)
 	len2 = printf("Percent:[%%]\n");
 	_printf("Len:[%d]\n", len);
 	printf("Len:[%d]\n", len2);
-	_printf("Unknown:[%r]\n");
+	_printf("Unknown:[%q]\n");
 	printf("Unknown:[%r]\n");
 	len = _printf("%i\n", INT_MIN);
 	len2 = printf("%i\n", INT_MIN);
